f2905f9c134e2f9766a68b9b55478f69229bd129
Shashank Shekhar <shashank7200@gmail.com>
2017-11-20 13:47:27 +0530

    added logic to check whther the script is loaded or not

--- js/iife1.js
+++ js/iife1.js
@@ -1,12 +1,16 @@
-(function(){
-  console.log("I am loaded again");
+(function () {
+  alert("I am loaded again");
+  if (document.querySelector('script[src="http://localhost:3000/api/v1/widgetgenerator"]')) {
+    alert("The script was already loaded");
+  }
+  else {
     const fileref = document.createElement('script');
-  
+
     fileref.setAttribute('type', 'text/javascript');
     fileref.setAttribute('src', "http://localhost:3000/api/v1/widgetgenerator");
-    
+
     if (typeof fileref !== 'undefined') {
       document.getElementsByTagName('head')[0].appendChild(fileref);
     }
-
+  }
 })();
