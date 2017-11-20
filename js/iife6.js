d06f668eef90b04ed5e473969765ccf0fd440944
Shashank Shekhar <shashank7200@gmail.com>
2017-11-20 17:35:25 +0530

    modified

--- js/iife6.js
+++ js/iife6.js
@@ -1,15 +1,21 @@
-(function () {
-  scripts  =["https://checkout.razorpay.com/v1/checkout.js", "http://localhost:3000/api/v1/widgetgenerator"];
-
+(function (apiKey2) {
+  scripts  =["https://checkout.razorpay.com/v1/checkout.js"];
+  console.log(apiKey)
+  console.log(apiKey2)
   scripts.forEach((scriptSrc) =>{
     if (document.querySelector('script[src=\"'+scriptSrc+'\"]')) {
       alert("The script was already loaded: "+scriptSrc);
     }
     else {
+      // noOfCalls = 
+      // btnMap = {
+      //   0: 'aaaa',
+      //   1: 'bbbb'
+      // }
+
       const fileref = document.createElement('script');
       fileref.setAttribute('type', 'text/javascript');
       fileref.setAttribute('src', scriptSrc);
-  
       if (typeof fileref !== 'undefined') {
         document.getElementsByTagName('head')[0].appendChild(fileref);
         alert("The script was added to head: "+scriptSrc);
@@ -17,4 +23,5 @@
     }
   });
   
-})();
+  
+})(apiKey);
