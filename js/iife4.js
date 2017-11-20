6d0e4b19edbb2a69fa68657f81686df628d7fed6
Shashank Shekhar <shashank7200@gmail.com>
2017-11-20 15:14:04 +0530

    renamed session event 4

--- js/iife4.js
+++ js/iife4.js
@@ -2,7 +2,7 @@
   scripts  =["https://checkout.razorpay.com/v1/checkout.js", "http://localhost:3000/api/v1/widgetgenerator"];
 
   scripts.forEach((scriptSrc) =>{
-    if (document.querySelector('script[src=scriptSrc]')) {
+    if (document.querySelector('script[src='+scriptSrc+']')) {
       alert("The script was already loaded: "+scriptSrc);
     }
     else {
